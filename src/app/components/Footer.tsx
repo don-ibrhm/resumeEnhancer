bf40e3980687384fdb47d51470f94cd006dd4591
donawk <don.ashebir@gmail.com>
2023-10-05 14:50:29 +0400

    pdf in view now scrollable, 'double footer' removed

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,6 +2,6 @@
 
 export const Footer = () => {
     return (
-        <footer className="bottom-0 h-4 py-3 my-8 border-t-2 border-gray-100 flex justify-center align-center">Powered by Diverge</footer>
+        <footer className="bottom-0 h-4 py-7 border-t-2 border-gray-100 flex justify-center items-center">Powered by Diverge</footer>
     )
 }
